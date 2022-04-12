ad86bcd7eb5928fc4d238d418c4d815cd393574b
canonn11 <myungkwanokk@gmail.com>
2022-04-12 17:59:30 +0900

    문단 파싱 및 Response template 구현

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
-from flask import request
+# pip freeze > requirements.txt     // 패키지 목록 생성
+# pip install -r requirements.txt     // 패키지 목록 읽어서 설치
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# 서버 작동 여부 확인 Route ('/')
 @app.route('/')
 def main_page():
-    return "This is main page."
+    return "Server Linked!"
+
+# Emotion Prediction Route ('/prediction')
+@app.route('/prediction', methods = ['POST'])
+def prediction():
+    # request를 json으로 받아온다.
+    req = request.get_json()
+    content = req['content']
+    
+    # 한 문장씩 파싱 ( 필요시 사용 )
+    text=content.replace('\n',' ').replace('  ',' ')
+    text=text.split('.')
+    #print(text)
+    
+    
+
+
+    # return example. ( 추후 수정 )
+    emotion = 'happy'
+    return jsonify(emotion= emotion)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
