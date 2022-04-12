c219d9041885ac7d66092f6928a446bf2d2e0f04
canonn11 <myungkwanokk@gmail.com>
2022-04-12 15:25:02 +0900

    프로젝트 초기 설정

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return "This is main page."
+
+if __name__ == '__main__':
+    app.run(host = 'localhost',port=5000)
